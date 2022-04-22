dd832070f84c1435288083494c148f53c7c5f0e1
Luana <Luana@DESKTOP-ODA6GJK>
2022-04-21 21:57:54 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaDeRepeticao_FOR_CSharp-09/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EstruturaDeRepeticao_FOR_CSharp_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Código simples para entendimento do laço FOR.
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
